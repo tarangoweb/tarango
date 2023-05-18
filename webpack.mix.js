863b08a9ba8edbb3bd2053c46b06763612b1af82
Will Garrison <willgarrison@gmail.com>
2023-05-18 18:13:00 -0400

    adjust build versioning

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,28 +5,25 @@ const FileIncludeWebpackPlugin = require("file-include-webpack-plugin");
 mix.setPublicPath("docs");
 
 mix
-  .js("src/scripts/main.js", "scripts")
-  .postCss("src/styles/main.css", "styles", [
-    require("tailwindcss"),
-    require("autoprefixer"),
-  ])
-  .minify("docs/scripts/main.js")
-  .minify("docs/styles/main.css")
-  .copy("src/static/*", "docs/")
-  .copy("src/assets", "docs/assets")
-  .webpackConfig((webpack) => {
-    return {
-      plugins: [
-        new FileIncludeWebpackPlugin({
-          source: "./src/templates/pages", // relative path to your templates
-          destination: "./", // relative path to your docs folder
-          replace: [
-            {
-              regex: /\[\[FILE_VERSION]]/, // additional things to replace
-              to: "v=1.0.0",
-            },
-          ],
-        }),
-      ],
-    };
-  });
+	.js("src/scripts/main.js", "scripts")
+	.postCss("src/styles/main.css", "styles", [require("tailwindcss"), require("autoprefixer")])
+	.minify("docs/scripts/main.js")
+	.minify("docs/styles/main.css")
+	.copy("src/static/*", "docs/")
+	.copy("src/assets", "docs/assets")
+	.webpackConfig((webpack) => {
+		return {
+			plugins: [
+				new FileIncludeWebpackPlugin({
+					source: "./src/templates/pages", // relative path to your templates
+					destination: "./", // relative path to your docs folder
+					replace: [
+						{
+							regex: /\[\[FILE_VERSION]]/, // additional things to replace
+							to: "v=" + Math.floor(new Date().getTime() / 1000),
+						},
+					],
+				}),
+			],
+		};
+	});
